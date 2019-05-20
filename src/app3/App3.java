ad61b0d2c526c2f6b8bf85995bf062eaaebe5515
Bruna <bruna.lima@zup.com.br>
2019-05-20 14:06:31 -0300

    Serve para mostrar diferença de valores

---
+++ src/app3/App3.java
@@ -0,0 +1,21 @@
+package app3;
+
+import java.util.Scanner;
+
+public class App3 {
+	public static void main(String[] args) {
+
+	Scanner entrada = new Scanner (System.in);
+	
+	System.out.println("informe o primeiro valor:");
+	int valorA = entrada.nextInt();
+	
+	
+	System.out.println("informe o segundo valor:");
+	int valorB = entrada.nextInt();
+	
+	if (valorA > valorB);
+	System.out.println("A diferença entre os dois valores é:" + (valorA - valorB));
+	
+	}
+}
